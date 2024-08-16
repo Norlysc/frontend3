a1422b2b75818b37fee5ad5c5f0895cd4ffd6524
Norlys Castañeda <castaneda30@hotmail.com>
2024-08-15 20:39:52 -0500

    creacion de componente card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 
-
-const Card = () => {
-  return <div>Hola, soy una tarjeta</div>;
+const Card = ({ nombre, descripcion }) => {
+  return (
+    <div className="card">
+      <h2>{nombre}</h2>
+      <p>{descripcion}</p>
+    </div>
+  );
 };
 
 export default Card;
